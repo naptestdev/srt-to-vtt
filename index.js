f5d74157a9715beeecda64826380dc0ed3480622
napthedev <phongna.dev@gmail.com>
2022-01-03 21:47:26 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+const { convert } = require("subtitle-converter");
+const axios = require("axios").default;
+
+app.get("/", async (req, res) => {
+  try {
+    if (!req.query.url)
+      return res.send("SRT to VTT Proxy. Params url is required");
+
+    const response = await axios.get(req.query.url);
+
+    if (response.headers["content-type"] !== "application/x-subrip")
+      return res.status(400).send("Invalid content type");
+
+    const { subtitle, status } = convert(response.data, ".vtt");
+
+    if (!status.success) return res.status(400).send("Cannot convert");
+
+    res.send(subtitle);
+  } catch (error) {
+    console.log(error);
+    if (!res.headerSent) res.sendStatus(500);
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
